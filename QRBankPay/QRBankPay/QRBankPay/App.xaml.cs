03ab4fc1ed3dc5307b4a8f5b9ab3cacdf9add13a
Juan Esteban Alvarez Ruiz <70873277+juanes030@users.noreply.github.com>
2023-07-29 09:54:48 -0500

    ajuste login
    elimnar lo que no se usa y ajuste en login

--- QRBankPay/QRBankPay/QRBankPay/App.xaml.cs
+++ QRBankPay/QRBankPay/QRBankPay/App.xaml.cs
@@ -1,8 +1,4 @@
-﻿using QRBankPay.Services;
-using QRBankPay.Views;
-using System;
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms;
 
 namespace QRBankPay
 {
@@ -13,7 +9,6 @@ namespace QRBankPay
         {
             InitializeComponent();
 
-            DependencyService.Register<MockDataStore>();
             MainPage = new AppShell();
         }
 
---
+++ QRBankPay/QRBankPay/QRBankPay/Data/Models/Client.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace QRBankPay.Data.Models
+{
+    public class Client
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Dna { get; set; } = string.Empty;
+    }
+
+}
+
