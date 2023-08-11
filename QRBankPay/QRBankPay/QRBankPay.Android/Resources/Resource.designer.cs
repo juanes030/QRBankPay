be118ca3d9ec9fa225c5b10dfec3b112790f2744
Juan Esteban Alvarez Ruiz <70873277+juanes030@users.noreply.github.com>
2023-08-10 21:18:39 -0500

    ajuste de splash
    se ajusta el splash para android

--- QRBankPay/QRBankPay/QRBankPay.Android/Resources/Resource.designer.cs
+++ QRBankPay/QRBankPay/QRBankPay.Android/Resources/Resource.designer.cs
@@ -22236,16 +22236,19 @@ namespace QRBankPay.Droid
 			public const int preference_list_divider_material = 2131165327;
 			
 			// aapt resource value: 0x7F070090
-			public const int test_custom_background = 2131165328;
+			public const int splash = 2131165328;
 			
 			// aapt resource value: 0x7F070091
-			public const int tooltip_frame_dark = 2131165329;
+			public const int test_custom_background = 2131165329;
 			
 			// aapt resource value: 0x7F070092
-			public const int tooltip_frame_light = 2131165330;
+			public const int tooltip_frame_dark = 2131165330;
 			
 			// aapt resource value: 0x7F070093
-			public const int xamarin_logo = 2131165331;
+			public const int tooltip_frame_light = 2131165331;
+			
+			// aapt resource value: 0x7F070094
+			public const int xamarin_logo = 2131165332;
 			
 			static Drawable()
 			{
