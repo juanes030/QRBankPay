182b7a2176b8aa9a67c8cb6d85dbab6239e20de3
Juan Esteban Alvarez Ruiz <70873277+juanes030@users.noreply.github.com>
2023-08-16 12:40:12 -0500

    configuracion de load
    configuracion de load

--- QRBankPay/QRBankPay/QRBankPay/Settings.cs
+++ QRBankPay/QRBankPay/QRBankPay/Settings.cs
@@ -2,7 +2,7 @@
 {
     public static class Settings
     {
-        public const string ApiBaseUri = "http://192.168.1.9:5000/api";
+        public const string ApiBaseUri = "http://10.101.1.100:5000/api";
     }
 }
 
--- QRBankPay/QRBankPay/QRBankPay/ViewModels/BaseViewModel.cs
+++ QRBankPay/QRBankPay/QRBankPay/ViewModels/BaseViewModel.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using Xamarin.CommunityToolkit.ObjectModel;
 using Xamarin.Forms;
 
 namespace QRBankPay.ViewModels
 {
-    public class BaseViewModel : INotifyPropertyChanged
+    public class BaseViewModel : ObservableObject
     {
         bool isBusy = false;
         public bool IsBusy
@@ -15,36 +16,23 @@ namespace QRBankPay.ViewModels
             set { SetProperty(ref isBusy, value); }
         }
 
+        bool isNotBusy = true;
+        public bool IsNotBusy
+        {
+            get => isNotBusy;
+            set
+            {
+                if (SetProperty(ref isNotBusy, value))
+                    IsBusy = !isNotBusy;
+            }
+        }
+
+
         string title = string.Empty;
         public string Title
         {
             get { return title; }
             set { SetProperty(ref title, value); }
-        }
-
-        protected bool SetProperty<T>(ref T backingStore, T value,
-            [CallerMemberName] string propertyName = "",
-            Action onChanged = null)
-        {
-            if (EqualityComparer<T>.Default.Equals(backingStore, value))
-                return false;
-
-            backingStore = value;
-            onChanged?.Invoke();
-            OnPropertyChanged(propertyName);
-            return true;
-        }
-
-        #region INotifyPropertyChanged
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
-        {
-            var changed = PropertyChanged;
-            if (changed == null)
-                return;
-
-            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-        #endregion
+        }       
     }
 }
